477ab1e7a22047390712b42179969030fe05eb6c
Theones777 <Zoolexi1995>
2022-04-20 17:58:11 +0300

    path categories

---
+++ testsite/women/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from women.views import *
+
+urlpatterns = [
+    path('', index),
+    path('cats/', categories),
+]
--- testsite/women/views.py
+++ testsite/women/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 
-def index(request): #HttpRequest
-    return HttpRequest('Страница приложения women')
+def index(request):  # HttpRequest
+    return HttpResponse('Страница приложения women')
+
+
+def categories(request):  # HttpRequest
+    return HttpResponse('<h1>States</h1>')
