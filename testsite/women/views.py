f19d434d5b613d2ed4add7707795b9e6f492d428
Theones777 <Zoolexi1995>
2022-05-08 10:07:22 +0300

    ForeignKey

--- testsite/women/views.py
+++ testsite/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from women.models import *
 
 menu = [{'title': 'About site', 'url_name': 'about'},
@@ -10,9 +10,14 @@ menu = [{'title': 'About site', 'url_name': 'about'},
 
 def index(request):  # HttpRequest
     posts = Women.objects.all()
+    cats = Category.objects.all()
+
     context = {'posts': posts,
+               'cats': cats,
                'menu': menu,
-               'title': 'MAIN PAGE'}
+               'title': 'MAIN PAGE',
+               'cat_selected': 0}
+
     return render(request, 'women/index.html', context=context)
 
 
@@ -35,6 +40,21 @@ def login(request):  # HttpRequest
 
 def show_post(request, post_id):  # HttpRequest
     return HttpResponse(f'Page with id = {post_id}')
+
+
+def show_category(request, cat_id):  # HttpRequest
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+    context = {'posts': posts,
+               'cats': cats,
+               'menu': menu,
+               'title': 'CATEGORY PAGE',
+               'cat_selected': cat_id}
+
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):  # HttpRequest
