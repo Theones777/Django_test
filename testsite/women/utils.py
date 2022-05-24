4605994044d4e31585ab0c9a76ee8a6c45dacf89
Theones777 <Zoolexi1995>
2022-05-24 10:35:19 +0300

    cache

--- testsite/women/utils.py
+++ testsite/women/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-
+from django.core.cache import cache
 from women.models import *
 
 menu = [{'title': 'About site', 'url_name': 'about'},
@@ -12,7 +12,10 @@ class DataMixin:
 
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('women'))
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
